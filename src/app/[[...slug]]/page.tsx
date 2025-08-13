bbcff33aa8be9e806ee6713a2001927fb7d38651
Iryna Serhiienko <irishka.sergienko@gmail.com>
2025-08-13 12:59:15 +0300

    сhore: update scripts, clean up

--- src/app/[[...slug]]/page.tsx
+++ src/app/[[...slug]]/page.tsx
@@ -2,6 +2,10 @@ import '../../index.css';
 
 import { ClientOnly } from './client';
 
+export function generateStaticParams() {
+  return [{ slug: [''] }];
+}
+
 export default function Page() {
   return <ClientOnly />;
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,9 @@
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'My App',
+  description: 'My App is a...',
+};
 export default function RootLayout({
   children,
 }: {
